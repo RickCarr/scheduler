46539bd500c5ca6a79ebf1fd1f4118c0086075d2
RickCarr <rick.carr@telus.com>
2023-01-10 19:50:58 -0800

    adding DayListItem with scss and a test to validate it

--- stories/index.js
+++ stories/index.js
@@ -6,11 +6,13 @@ import { action } from "@storybook/addon-actions";
 import "index.scss";
 
 import Button from "components/Button";
+import DayListItem from "components/DayListItem";
+
 
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
-  })  
+  })
   .add("Base", () => <Button>Base</Button>)
   .add("Confirm", () => <Button confirm>Confirm</Button>)
   .add("Danger", () => <Button danger>Cancel</Button>)
@@ -20,5 +22,15 @@ storiesOf("Button", module)
   .add("Disabled", () => (
     <Button disabled onClick={action("button-clicked")}>
       Disabled
-    </Button>    
+    </Button>
+  ));
+storiesOf("DayListItem", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Unselected", () => <DayListItem name="Monday" spots={5} />) 
+  .add("Selected", () => <DayListItem name="Monday" spots={5} selected />)
+  .add("Full", () => <DayListItem name="Monday" spots={0} />)
+  .add("Clickable", () => (
+    <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> 
   ));
